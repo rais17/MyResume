27b58bf3a98c5d615f09fb5618c84c5d9ca41730
Arshad Rais <86293309+rais17@users.noreply.github.com>
2023-03-27 13:32:41 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Robo: ["Roboto", "sans-serif"],
+        Pop: ["Poppins", "sans-serif"],
+        mullish: ["Mulish", "sans-serif"],
+        Osw: ["Oswald", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
